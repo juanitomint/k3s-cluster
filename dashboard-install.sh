3286c45f35b30a83aaf810b2a426b46dc555c935
Juan Borda <juanignacioborda@gmail.com>
2019-09-25 22:52:38 -0300

    add dashboard

---
+++ dashboard-install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+helm install \
+--set enableInsecureLogin=true,enableSkipLogin=true,rbac.clusterAdminRole=true,ingress.enabled=true \
+--name kubernetes-dashboard \
+--namespace kubernetes-dashboard \
+--wait --timeout=90 \
+stable/kubernetes-dashboard
+
+POD_NAME=$(kubectl get pods -n kubernetes-dashboard -l "app=kubernetes-dashboard,release=kubernetes-dashboard" -o jsonpath="{.items[0].metadata.name}")
+echo http://127.0.0.1:9090/
+kubectl -n kubernetes-dashboard port-forward $POD_NAME 9090:9090
