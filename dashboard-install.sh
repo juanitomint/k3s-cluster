e6eed8abb8abb2dd3abb91ec030f697faf2b6c6c
juan Ignacio Borda <juanignacioborda@gmail.com>
2019-11-12 13:09:19 -0300

    expose port 9000 for dashboard

--- dashboard-install.sh
+++ dashboard-install.sh
@@ -7,5 +7,5 @@ helm install \
 stable/kubernetes-dashboard
 
 POD_NAME=$(kubectl get pods -n kubernetes-dashboard -l "app=kubernetes-dashboard,release=kubernetes-dashboard" -o jsonpath="{.items[0].metadata.name}")
-echo http://127.0.0.1:9090/
-kubectl -n kubernetes-dashboard port-forward $POD_NAME 9090:9090
+echo http://127.0.0.1:9000/
+kubectl -n kubernetes-dashboard port-forward $POD_NAME 9000:9090
