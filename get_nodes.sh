14cc926e4ee88c60ae318531589912455dfe0c2a
juan Ignacio Borda <juanignacioborda@gmail.com>
2019-11-12 12:18:05 -0300

    layer 7 load balancer WIP1

--- get_nodes.sh
+++ get_nodes.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 NODES=$(kubectl get nodes -o jsonpath='{ $.items[*].status.addresses[?(@.type=="InternalIP")].address }')
 
+cat traefik_config/dynamic_conf.tmpl > traefik_config/dynamic_conf.toml
+# IFS=" "
+template=$(cat traefik_config/dynamic_hosts.tmpl)
+echo "template:"
+echo "$template"
 echo -e "found this nodes:"
 for node in ${NODES} ; do
 echo $node
+url="http://$node"
+echo "url: $url" 
+echo "${template//nodeurl/$url}" >> traefik_config/dynamic_conf.toml
 done
