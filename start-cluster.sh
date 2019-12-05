42c5b9fd895f04d9ed8bcb47c77e988ff484ac28
juan Ignacio Borda <juanignacioborda@gmail.com>
2019-12-05 10:18:00 -0300

    implements scale + L7 proxy nginx

--- start-cluster.sh
+++ start-cluster.sh
@@ -3,8 +3,20 @@ if [ "$0" = "$BASH_SOURCE" ]; then
     echo "Error: Script must be run with source:"
     echo "source ./start-cluster.sh"
     exit 1
+fi    
+
+# set scale
+SCALE=4
+
+if [ "$1" ]; then 
+SCALE=$1
 fi
+
+
+echo "SCALE: $SCALE"
 echo "starting server: docker-compose up -d"
-docker-compose up -d --remove-orphans
+docker-compose up -d --remove-orphans --scale worker_node=$SCALE
 echo -e "\nloading profile in KUBECONFIG AS: default"
-export KUBECONFIG=$(pwd)/kubeconfig.yaml 
+export KUBECONFIG=$KUBECONFIG:$(pwd)/kubeconfig.yaml 
+
+docker-compose ps
